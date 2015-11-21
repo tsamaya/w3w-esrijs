f0d931cd506f5b200eb79532ecaa179ab2b8b2a3
arnaud ferrand <arferrand@gmail.com>
2015-11-21 16:18:50 +0100

    link to developer.what3words.com

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 define([], function() {
   return {
-    key : eval(String.fromCharCode(34, 81, 52, 77, 53, 49, 87, 74, 90, 34)) // this is my key !
+    // this is my key ! sign up of for yours, it is free http://developer.what3words.com
+    key : eval(String.fromCharCode(34, 81, 52, 77, 53, 49, 87, 74, 90, 34))
   }
 });
